494e704fc6d70a347f649fa99cd62565383c362b
Yury V Bukhman <ybukhman@morgridge.org>
2022-07-07 12:14:44 -0500

    Adapted differential_copy_number.R to version 4 of GeneDups.tsv

--- differential_copy_number.R
+++ differential_copy_number.R
@@ -9,7 +9,7 @@
 library(tidyverse)
 
 # Parameters
-WORK_DIR <- "/Volumes/home/Projects/BWGENOME/final_stage_analyses/Gene_lists/Version_2/2_differential_copy_number"
+WORK_DIR <- "/Volumes/home/Projects/BWGENOME/final_stage_analyses/Gene_lists/2_differential_copy_number/Version_3"
 GENE_COPY_NUMBERS_FILE <- "GeneDups.tsv"
 GENES_OF_INTEREST_FILE <- "/Volumes/home/Projects/BWGENOME/final_stage_analyses/Gene_lists/1_body_size_and_development/genes_of_interest_scored_by_evidence.csv"
 FOLD_CHANGE_CUTOFF <- 2
@@ -17,12 +17,12 @@ setwd(WORK_DIR)
 
 # Find genes that satisfy the fold change cutoff
 segdups <- read_tsv(GENE_COPY_NUMBERS_FILE)
-fold_change <- (segdups$Blue_whale+2)/(segdups$Vaquita+2)
+fold_change <- (segdups$BlueWhale)/(segdups$Vaquita)
 diff_copy_num <- abs(log2(fold_change)) >= abs(log2(FOLD_CHANGE_CUTOFF))
 diffdups <- segdups[diff_copy_num,]
 write_csv(diffdups, "diff_copy_num_genes.csv")
 
 # Intersect with genes of interest
 genes_of_interest <- read_csv(GENES_OF_INTEREST_FILE)
-diff_dup_genes_of_interest <- merge(diffdups, genes_of_interest, by = "gene")
+diff_dup_genes_of_interest <- merge(diffdups, genes_of_interest, by.x = "Gene", by.y = "gene")
 diff_dup_genes_of_interest %>% arrange(desc(score)) %>% write_csv("diff_copy_num_genes_of_interest.csv")
